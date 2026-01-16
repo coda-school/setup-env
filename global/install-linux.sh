55b5a5c3606b5c5332945383de7dad4234f5d921
Yoan Thirion <yoan.thirion@gmail.com>
2026-01-16 13:46:49 +0100

    feat: remove useless stuff

--- global/install-linux.sh
+++ global/install-linux.sh
@@ -16,19 +16,6 @@ has() { command -v "$1" >/dev/null 2>&1; }
 # --- Mise à jour des paquets ---
 info "Mise à jour des paquets système..."
 sudo apt update && sudo apt upgrade -y || err "Échec mise à jour des paquets"
-sudo apt install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
 
 # --- Git et GitHub Desktop ---
 if ! has git; then
